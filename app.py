f439871f013d4fdee224f139ff8c1fbc032d3aec
solithv <120559288+solithv@users.noreply.github.com>
2023-07-20 00:31:23 +0900

    reward仮

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def main():
     fields = glob.glob("./field_data/*.csv")
     env = gym.make(
         "TaniJoh-v0",
-        max_episode_steps=10,
+        # max_episode_steps=10,
         csv_path=random.choice(fields),
         render_mode="human",
         controller="pygame",
@@ -17,11 +17,8 @@ def main():
     terminated, truncated = [False] * 2
     while not terminated and not truncated:
         env.render()
-        # terminated: エピソード終了フラグ
-        # truncated: ステップ数上限での終了フラグ
-        observation, reward, terminated, truncated, _ = env.step(
-            env.get_actions_from_render()
-        )
+        observation, reward, terminated, truncated, _ = env.step(env.get_actions())
+        print(f"reward:{reward}")
     env.close()
 
 
