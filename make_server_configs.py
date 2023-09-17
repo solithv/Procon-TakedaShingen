3b56e85328548cd75a51d24b378406610ed7f474
solithv <120559288+solithv@users.noreply.github.com>
2023-09-17 17:46:07 +0900

    update config

--- make_server_configs.py
+++ make_server_configs.py
@@ -5,19 +5,22 @@ import re
 from pathlib import Path
 
 import numpy as np
+from dotenv import load_dotenv
 
+load_dotenv()
+token = os.getenv("TOKEN")
 teams = {
-    "first": {"name": "先攻チーム", "token": "先攻チームのトークン"},
+    "first": {"name": "先攻チーム", "token": token},
     "second": {"name": "後攻チーム", "token": "後攻チームのトークン"},
 }
 turns = {11: 30, 13: 54, 15: 80, 17: 100, 21: 150, 25: 200}
 seconds = {11: 3, 13: 4, 15: 6, 17: 8, 21: 11, 25: 15}
 bonus = {"wall": 10, "territory": 30, "castle": 100}
+csv_dir = "./field_data"
+config_dir = "./server_configs"
 
 
 def make_configs():
-    csv_dir = "./field_data"
-    config_dir = "./server_configs"
     for csv_ in Path(csv_dir).glob("*.csv"):
         data = {}
         size = int(re.sub(r"[\D]", "", os.path.normpath(csv_).split(os.path.sep)[-1]))
