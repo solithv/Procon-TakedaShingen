3d20f49ee32f2a84b3205f1199de886bdc79ca02
moririnUs <moririn429@gmail.com>
2023-09-17 17:34:46 +0900

    API_update

--- MyEnv/Field_API.py
+++ MyEnv/Field_API.py
@@ -1,36 +1,44 @@
-import requests
-import json
+import requests as req
 import os
-import numpy
+import json
 from dotenv import load_dotenv
-class Packing:
+class API:
 
-    req = requests
     #.envはトークンを格納しているファイル
     load_dotenv(".env")
     token = os.getenv("TOKEN")
     match_url = os.getenv("MATCH_URL")
-    map_url = []
     header = {"procon-token":token}
     query = {"token":token}
-
+    
     def get_match(self):
         """
         試合一覧取得API
+        返り値: 試合idを格納した行列
         """
-        match = self.req.get(self.match_url, self.token)
+        r = req.get(self.match_url, headers=self.header)
 
-        for num in range(len(match.matches)):
-            self.map_url.append(self.match_url + str(match[num]))
+        matches = r.json()
+        id_ = []
+        for match in matches["matches"]:
+            id_.append(f'/{match["id"]}')
+        return id_
     
-    def get_map(self,match_num):
+    def get_field(self,path):
         """
         試合状態取得API
+        引数(試合id)
         """
-        map = self.req.get()
-        return (map.board,)
+        r = req.get(f"{self.match_url}/{path}",headers=self.header)
+        field = r.json()
+        return field
 
-    def post_act(self,next_turn,acts):
-        self.req.post(self.url,data = self.data,header = self.header,turn = next_turn,actions = acts)
+    def post_actions(self,act,path):
+        """
+        行動計画更新API
+        引数(jsonファイル , 試合id)
+        """
+
+        req.post(f"{self.match_url}/{path}", headers=self.header, json=act)
 
         
