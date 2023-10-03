ed8cfaf25748ddea1fee218380fecece0e93f460
solithv <120559288+solithv@users.noreply.github.com>
2023-10-03 12:37:02 +0900

    wip

--- NN/nn.py
+++ NN/nn.py
@@ -13,21 +13,38 @@ from Utils import Util
 from .dataset import DatasetUtil
 
 
-class PositionalEmbedding(layers.Layer):
-    def __init__(self, sequence_length, input_dim, output_dim):
-        super(PositionalEmbedding, self).__init__()
-        self.pos_embedding = self.add_weight(
-            "pos_embedding",
-            shape=(1, sequence_length, input_dim),
-            initializer=tf.keras.initializers.RandomNormal(),
-            trainable=True,
-        )
-        self.input_dim = input_dim
-        self.output_dim = output_dim
-        print(self.pos_embedding)
+class Patches(layers.Layer):
+    def __init__(self, patch_size):
+        super().__init__()
+        self.patch_size = patch_size
 
-    def call(self, inputs):
-        return inputs + self.pos_embedding
+    def call(self, images):
+        batch_size = tf.shape(images)[0]
+        patches = tf.image.extract_patches(
+            images=images,
+            sizes=[1, self.patch_size, self.patch_size, 1],
+            strides=[1, self.patch_size, self.patch_size, 1],
+            rates=[1, 1, 1, 1],
+            padding="VALID",
+        )
+        patch_dims = patches.shape[-1]
+        patches = tf.reshape(patches, [batch_size, -1, patch_dims])
+        return patches
+
+
+class PatchEncoder(layers.Layer):
+    def __init__(self, num_patches, projection_dim):
+        super().__init__()
+        self.num_patches = num_patches
+        self.projection = layers.Dense(units=projection_dim)
+        self.position_embedding = layers.Embedding(
+            input_dim=num_patches, output_dim=projection_dim
+        )
+
+    def call(self, patch):
+        positions = tf.range(start=0, limit=self.num_patches, delta=1)
+        encoded = self.projection(patch) + self.position_embedding(positions)
+        return encoded
 
 
 class NNModel:
@@ -39,83 +56,78 @@ class NNModel:
         """
         input_shape = (len(Game.CELL[: Game.CELL.index("worker_A0")]) + 2, sides, sides)
         output_size = len(Game.ACTIONS)
-        self.model = self.define_model(input_shape, output_size)
+        self.model = self.define_model(input_shape, output_size, patch_size=5)
 
     def define_model(
         self,
-        input_shape: tuple[int],
-        output_dim: int,
-        num_heads: int = 4,
-        ff_dim: int = 32,
-        num_layers: int = 2,
+        input_shape,
+        num_classes,
+        learning_rate=0.001,
+        weight_decay=0.0001,
+        batch_size=256,
+        num_epochs=100,
+        image_size=72,  # We'll resize input images to this size
+        patch_size=6,  # Size of the patches to be extract from the input images
+        projection_dim=64,
+        num_heads=4,
+        transformer_layers=8,
+        mlp_head_units=[2048, 1024],  # Size of the dense layers of the final classifier
     ):
-        """モデルを定義
+        def mlp(x, hidden_units, dropout_rate):
+            for units in hidden_units:
+                x = layers.Dense(units, activation=tf.nn.gelu)(x)
+                x = layers.Dropout(dropout_rate)(x)
+            return x
 
-        Args:
-            input_shape (tuple[int]): 入力の形状
-            output_dim (int): 出力の次元数
-            num_heads (int, optional): マルチヘッドアテンションのヘッド数. Defaults to 4.
-            ff_dim (int, optional): フィードフォワードネットワークの次元数. Defaults to 32.
-            num_layers (int, optional): トランスフォーマーレイヤーの数. Defaults to 2.
-        """
+        def create_vit_classifier(input_shape, num_classes):
+            inputs = layers.Input(shape=input_shape)
+            print(inputs)
+            preshape = tf.transpose(inputs, (0, 2, 3, 1))
+            print(preshape)
 
-        def transformer_layer(
-            input_shape, input_dim, num_heads, ff_dim, dropout_rate=0.1
-        ):
-            inputs = keras.Input(shape=(None, input_dim))
-            print(input_shape)
-            x = PositionalEmbedding(input_shape[0], input_dim, input_dim)(inputs)
-            x = keras.layers.MultiHeadAttention(
-                key_dim=input_dim, num_heads=num_heads, dropout=dropout_rate
-            )(x, x)
-            x = keras.layers.LayerNormalization(epsilon=1e-6)(x)
-            x = keras.layers.Dropout(dropout_rate)(x)
+            # Create patches.
+            patches = Patches(patch_size)(preshape)
+            # Encode patches.
+            encoded_patches = PatchEncoder(num_patches, projection_dim)(patches)
 
-            res = x + inputs
-            x = keras.layers.Conv1D(filters=ff_dim, kernel_size=1, activation="relu")(
-                res
+            # Create multiple layers of the Transformer block.
+            for _ in range(transformer_layers):
+                # Layer normalization 1.
+                x1 = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+                # Create a multi-head attention layer.
+                attention_output = layers.MultiHeadAttention(
+                    num_heads=num_heads, key_dim=projection_dim, dropout=0.1
+                )(x1, x1)
+                # Skip connection 1.
+                x2 = layers.Add()([attention_output, encoded_patches])
+                # Layer normalization 2.
+                x3 = layers.LayerNormalization(epsilon=1e-6)(x2)
+                # MLP.
+                x3 = mlp(x3, hidden_units=transformer_units, dropout_rate=0.1)
+                # Skip connection 2.
+                encoded_patches = layers.Add()([x3, x2])
+
+            # Create a [batch_size, projection_dim] tensor.
+            representation = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+            representation = layers.Flatten()(representation)
+            representation = layers.Dropout(0.5)(representation)
+            # Add MLP.
+            features = mlp(
+                representation, hidden_units=mlp_head_units, dropout_rate=0.5
             )
-            x = keras.layers.Dropout(dropout_rate)(x)
-            x = keras.layers.Conv1D(filters=input_dim, kernel_size=1)(x)
+            # Classify outputs.
+            logits = layers.Dense(num_classes)(features)
+            # Create the Keras model.
+            model = keras.Model(inputs=inputs, outputs=logits)
+            return model
 
-            output = x + res
-            return keras.Model(inputs, output)
+        num_patches = (image_size // patch_size) ** 2
+        transformer_units = [
+            projection_dim * 2,
+            projection_dim,
+        ]  # Size of the transformer layers
 
-        def build_transformer(
-            input_shape, num_layers, num_heads, ff_dim, output_dim, dropout_rate=0.1
-        ):
-            inputs = keras.Input(shape=input_shape)
-            x = tf.transpose(inputs, (0, 2, 3, 1))
-            input_shape = tuple(input_shape[i] for i in (1, 2, 0))
-            for _ in range(num_layers):
-                x = transformer_layer(
-                    input_shape, input_shape[-1], num_heads, ff_dim, dropout_rate
-                )(x)
-            x = keras.layers.GlobalAveragePooling1D()(x)
-            x = keras.layers.Dense(output_dim, activation="softmax")(x)
-            return keras.Model(inputs, x)
-
-        model = build_transformer(
-            input_shape, num_layers, num_heads, ff_dim, output_dim
-        )
-        return model
-
-        # inputs = keras.Input(input_shape)
-        # x = tf.transpose(inputs, (0, 2, 3, 1))
-        # # x = layers.Conv2D(64, (5, 5), padding="same", activation="relu")(x)
-        # x = layers.Conv2D(16, (3, 3), padding="same", activation="relu")(x)
-        # x = layers.SpatialDropout2D(0.2)(x)
-        # # x = layers.Dropout(0.2)(x)
-        # # x = layers.Conv2D(128, (3, 3), padding="same", activation="relu")(x)
-        # x = layers.Conv2D(32, (3, 3), padding="same", activation="relu")(x)
-        # # x = layers.Dropout(0.5)(x)
-        # x = layers.Flatten()(x)
-        # x = layers.Dense(64, activation="relu")(x)
-        # x = layers.Dropout(0.5)(x)
-        # # x = layers.Dense(32, activation="relu")(x)
-        # outputs = layers.Dense(output_size, activation="softmax")(x)
-
-        # return models.Model(inputs=inputs, outputs=outputs)
+        return create_vit_classifier(input_shape, num_classes)
 
     def save_model(self, model_dir: str, model_name: str):
         """モデルを保存
