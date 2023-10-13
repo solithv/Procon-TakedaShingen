2971999f5f4433e3b5fbc26ace03fcd62dfdba97
solithv <120559288+solithv@users.noreply.github.com>
2023-10-13 23:46:55 +0900

    change wait key

--- MyEnv/game.py
+++ MyEnv/game.py
@@ -1668,7 +1668,7 @@ class Game:
                         if keys[pygame.K_RIGHT]:
                             cellX += 1
 
-                    if event.key == pygame.K_0:
+                    if event.key == pygame.K_BACKSLASH:
                         y, x = self.workers[self.current_team][
                             actingWorker
                         ].get_coordinate()
